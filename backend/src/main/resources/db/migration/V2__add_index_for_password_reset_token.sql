8995a0ec2a1e9cb7bef57e35ff90881b5c2f1c8c
ardit203 <arditselmani203@gmail.com>
2026-04-11 17:02:12 +0200

    Updated PasswrodResetToken indexes

--- backend/src/main/resources/db/migration/V2__add_index_for_password_reset_token.sql
+++ backend/src/main/resources/db/migration/V2__add_index_for_password_reset_token.sql
@@ -3,3 +3,8 @@ create index idx_password_reset_tokens_user_id
 
 create index idx_password_reset_tokens_expires_at
     on password_reset_tokens(expires_at);
+
+create index idx_prt_active_by_user
+    on password_reset_tokens (user_id, expires_at)
+    where used_at is null
+      and invalidated_at is null;
